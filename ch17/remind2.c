fb5831a31c75c9b616c3c1bd8bf3461fdc03cb4c
Javier <dummylanguages@gmail.com>
2021-10-21 17:39:45 +0300

    add remind2.c example - chapter 17

---
+++ ch17/remind2.c
@@ -0,0 +1,69 @@
+/* Prints a one-month reminder list. */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_REMIND	50	/* Maximum number of reminders */
+#define MSG_LEN		60	/* Maximum length of reminder message */
+
+int		read_line(char str[], int n);
+
+int		main(void)
+{
+	char	*reminders[MAX_REMIND];
+	char	day_str[3];
+	char	msg_str[MSG_LEN + 1];
+	int		num_remind;
+	int		day;
+	int		i;
+	int		j;
+
+	num_remind = 0;
+	while (1)
+	{
+		if (num_remind == MAX_REMIND)
+		{
+			printf("-- No space left --\n");
+			break;
+		}
+		printf("Enter day and reminder: ");
+		scanf("%2d", &day);
+		if (day == 0)
+			break;
+		sprintf(day_str, "%2d", day);
+		read_line(msg_str, MSG_LEN);
+		for (i = 0; i < num_remind; i++)
+			if (strcmp(day_str, reminders[i]) < 0)
+				break;
+		for (j = num_remind; j > i; j--)
+			reminders[j] = reminders[j - 1];
+		reminders[i] = malloc(2 + strlen(msg_str) + 1);
+		if (reminders[i] == NULL)
+		{
+			printf("-- No space left --\n");
+			break;
+		}
+		strcpy(reminders[i], day_str);
+		strcat(reminders[i], msg_str);
+		num_remind++;
+	}
+	printf("\nDay Reminder\n");
+	for (i = 0; i < num_remind; i++)
+		printf("%s\n", reminders[i]);
+	return (0);
+}
+
+int		read_line(char str[], int n)
+{
+	int ch;
+	int i = 0;
+
+	while ((ch = getchar()) != '\n')
+	{
+		if (i < n)
+			str[i++] = ch;
+	}
+	str[i] = '\0';
+	return (i);
+}
